70d7b353e304ca620af540e9400b7c7d52e455a8
wldnjs981128@gmail.com <87362880+jiwonnnnnnn@users.noreply.github.com>
2023-07-13 10:07:31 +0900

    add gitignore

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') 
 def hello_world():
     return 'Hello, GitHub Action!'
+
+if __name__ == '__main__':
+    app.run()
